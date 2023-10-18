9a66fe62dd9e50fee76df7a2bf4d4e4b4e9a3071
Matias Bontempo <matias.bontempo@gmail.com>
2023-10-17 21:44:32 -0300

    feat: model view projection

--- src/main.cpp
+++ src/main.cpp
@@ -80,11 +80,15 @@ int main() {
     IndexBuffer ib(indices, 6);
 
     glm::mat4 proj = glm::ortho(0.0f, (float)WIDTH, 0.0f, (float)HEIGHT, -1.0f, 1.0f);
+    glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100, 0, 0));
+    glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200, 200, 0));
+
+    glm::mat4 mvp = proj * view * model;
 
     Shader shader("res/shaders/basic.shader");
     shader.Bind();
     shader.SetUniform4f("u_Color", 0.2f, 0.3f, 0.8f, 1.0f);
-    shader.SetUniformMat4f("u_MVP", proj);
+    shader.SetUniformMat4f("u_MVP", mvp);
 
     Texture texture("res/textures/test.png");
     texture.Bind();
